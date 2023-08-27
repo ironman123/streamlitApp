78240550df90bcf14aaa2442764218a159750c0d
Rajul Yadav <33024272+ironman123@users.noreply.github.com>
2023-08-27 21:35:40 +0530

    all retracted

--- streamlit.py
+++ streamlit.py
@@ -5,43 +5,30 @@ def find_largest_number(num1, num2, num3):
     return max(num1, num2, num3)
 
 # Set page title and layout
-st.set_page_config(page_title="Largest Number Finder", layout="wide")
-
-# Define custom CSS for the background image
-background_style = """
-<style>
-body {
-    background-image: url('https://img.goodfon.com/original/1920x1080/8/70/anime-girl-sexy-bikini-drinking-poolside-hot.jpg');
-    background-size: cover;
-    background-repeat: no-repeat;
-}
-</style>
-"""
-# Insert the custom CSS using Markdown
-st.markdown(background_style, unsafe_allow_html=True)
+st.set_page_config(page_title="Largest Number Finder", layout="centered")
 
 # Page header with animation
 st.title("🔍 Find the Largest Number")
 st.markdown("Enter three numbers and discover the **largest** among them.")
 
 # Input fields for three numbers
-num1 = st.number_input("Enter the first number:")
-num2 = st.number_input("Enter the second number:")
-num3 = st.number_input("Enter the third number:")
+num1 = st.number_input("Enter the First number:")
+num2 = st.number_input("Enter the Second number:")
+num3 = st.number_input("Enter the Third number:")
 
 # Button with animation
-if st.button("Find Largest"):
-    with st.spinner("Calculating..."):
-        time.sleep(2)  # Simulating calculation delay
+if st.button("Find The Largest!"):
+    with st.spinner("Calculating....."):
+        time.sleep(1)  # Simulating calculation delay
 
     st.markdown("---")  # Divider for a clean transition
 
     # Check for equality
     if num1 == num2 == num3:
-        st.error("All three numbers are equal.")
+        st.error("All three are equal.")
     else:
         largest = find_largest_number(num1, num2, num3)
         st.success(f"The largest number is: **{largest}**")
 
-        # Party emojis animation
+        # Animated emoji
         st.write("🎉🥳🎊")
