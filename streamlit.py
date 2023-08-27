903c3f9d3d41b6937156b7cd13bcbb09bbd9886a
Rajul Yadav <33024272+ironman123@users.noreply.github.com>
2023-08-27 21:19:15 +0530

    some more animation changes

--- streamlit.py
+++ streamlit.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import time
 
 def find_largest_number(num1, num2, num3):
     return max(num1, num2, num3)
@@ -7,8 +8,8 @@ def find_largest_number(num1, num2, num3):
 st.set_page_config(page_title="Largest Number Finder", layout="centered")
 
 # Page header with animation
-st.title("🔍 Find the Largest Number of the 3")
-st.write("Enter three numbers and discover the largest among them.")
+st.title("🔍 Find the Largest Number")
+st.markdown("Enter three numbers and discover the **largest** among them.")
 
 # Input fields for three numbers
 num1 = st.number_input("Enter the first number:")
@@ -17,16 +18,17 @@ num3 = st.number_input("Enter the third number:")
 
 # Button with animation
 if st.button("Find Largest"):
-    st.info("Calculating...")
-    st.empty()  # Clear previous content
+    with st.spinner("Calculating..."):
+        time.sleep(2)  # Simulating calculation delay
 
-    # Delay for animation effect
-    with st.spinner("Crunching the numbers..."):
-        st.empty()
+    st.markdown("---")  # Divider for a clean transition
 
-        # Check for equality
-        if num1 == num2 == num3:
-            st.error("All three numbers are equal.")
-        else:
-            largest = find_largest_number(num1, num2, num3)
-            st.success(f"The largest number is: {largest}")
+    # Check for equality
+    if num1 == num2 == num3:
+        st.error("All three numbers are equal.")
+    else:
+        largest = find_largest_number(num1, num2, num3)
+        st.success(f"The largest number is: **{largest}**")
+
+        # Animated emoji
+        st.write("🎉🎉🎉")
