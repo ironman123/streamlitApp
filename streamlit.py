99ecf050268f614b52a7c17c261ea9e54f5c77cd
ironman123 <rajulyadav123@gmail.com>
2023-08-27 20:51:50 +0530

    Initialization

---
+++ streamlit.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import streamlit as st
+
+
+num1 = st.text_input(label = "1st Number: ")
+num2 = st.text_input(label = "2nd Number: ")
+num3 = st.text_input(label = "3rd Number: ")
